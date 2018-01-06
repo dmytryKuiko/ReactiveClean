110552593065b34f628cd977c12d17a70b735cbc
dmytryK <madj90@gmail.com>
2018-01-06 17:18:57 +0100

    Timber planted

--- app/src/main/java/com/example/dimi/reactiveclean/App.kt
+++ app/src/main/java/com/example/dimi/reactiveclean/App.kt
@@ -2,11 +2,13 @@ package com.example.dimi.reactiveclean
 
 import android.app.Application
 import com.example.dimi.reactiveclean.utils.ComponentManager
+import timber.log.Timber
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         ComponentManager.initAppComponent(this)
     }
 }
